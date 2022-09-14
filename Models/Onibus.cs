6686b515881df7b95f67577e2e26b3ecf38640dc
fkcx <gabriel121oliveira@hotmail.com>
2022-09-14 15:06:59 -0300

    criacao de classe

--- Models/Onibus.cs
+++ Models/Onibus.cs
@@ -4,6 +4,7 @@
     {
         int _idOnibus;
         int _qtnpneus;
+
         public int IdOnibus { get => _idOnibus; set => _idOnibus = value; }
         public int Qtnpneus { get => _qtnpneus; set => _qtnpneus = value; }
     }
