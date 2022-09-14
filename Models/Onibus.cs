52b292c26b6c73bb7260eb649315b3348298d54b
fkcx <gabriel121oliveira@hotmail.com>
2022-09-14 12:03:01 -0300

    criação da classe e atributos

---
+++ Models/Onibus.cs
@@ -0,0 +1,10 @@
+﻿namespace Teste.Models
+{
+    public class Onibus
+    {
+        int _idOnibus;
+        int _qtnpneus;
+        public int IdOnibus { get => _idOnibus; set => _idOnibus = value; }
+        public int Qtnpneus { get => _qtnpneus; set => _qtnpneus = value; }
+    }
+}
